ed10f6a53efbd70ddb0625e4bb2871cfda71e231
Fernando <fh.rodrigues@outlook.com>
2015-09-30 19:43:03 -0300

    commit inicial

---
+++ ecclesia/src/main/java/br/com/ecclesia/MainServer.java
@@ -0,0 +1,18 @@
+package br.com.ecclesia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class MainServer extends SpringBootServletInitializer {
+	public static void main(String[] args) {
+		SpringApplication.run(MainServer.class, args);
+	}
+
+	
+}
